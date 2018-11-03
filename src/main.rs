b02a639f88b60adfa7a0141f9bc09be1472cf198
Sebastian Thiel <byronimo@gmail.com>
2018-11-03 13:22:59 +0100

    Input handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
-use std::{process, io::{self, stdin, stdout, BufRead, Write}};
+use std::{process, io::{self, stdin, stdout, BufRead}};
 
-fn main() -> Result<(), io::Error> {
+#[derive(Debug)]
+enum Error {
+    InvalidDimensions(&'static str, String),
+    InvalidCyphertext(&'static str, String),
+    IO(io::Error)
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Error::IO(err)
+    }
+}
+
+fn parse_dimensions(input: &str) -> Result<(u64, u64), Error> {
+    let mut ws = input.split_whitespace();
+    match (ws.next(), ws.next()) {
+        (Some(cls), Some(tss)) => {
+            Ok((
+                cls.parse().map_err(|_| Error::InvalidDimensions("could not parse cypher length", input.to_owned()))?,
+                tss.parse().map_err(|_| Error::InvalidDimensions("coudl not parse table size", input.to_owned()))?,
+            ))
+        },
+        _ => Err(Error::InvalidDimensions("need two whitespace separated tokens", input.to_owned())),
+    }
+}
+
+fn validated_cypher_text(input: &str, cypher_len: u64) -> Result<&str, Error> {
+    if input.len() < cypher_len as usize {
+        return Err(Error::InvalidCyphertext("cypher text is shorter than advertised", input.to_owned()))
+    }
+    Ok(&input[..cypher_len as usize])
+}
+
+fn main() -> Result<(), Error> {
     let (stdin, stdout) = (stdin(), stdout());
     let (mut stdin_lock, mut stdout_lock) = (stdin.lock(), stdout.lock());
 
@@ -18,7 +51,11 @@ fn main() -> Result<(), io::Error> {
                 eprintln!("input exhausted prematurely");
                 process::exit(2)
             }
-            _ => unimplemented!();
+            _ => {
+                let (cyper_len, table_size) = parse_dimensions(&first_line)?;
+                let cypher_text = validated_cypher_text(&second_line, cyper_len)?;
+                unimplemented!("{} {} {}", cyper_len, table_size, cypher_text)
+            }
         }
     }
 }
